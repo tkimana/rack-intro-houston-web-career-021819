8ac17c4d21fcf5999a0c44d17ee3be34e0f29c45
Theodore Kimana <kimana.theodore@gmail.com>
2019-03-11 17:33:44 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,9 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World".include "my name is"
+    resp.write "Hello, World"
     resp.finish
   end
 
 end
 
+def call(env)
+  resp = Rake::Response.new
+  resp.write "Hello,"
